36f9ae329e2984f58177c756d629232c109a6879
Yevgeniy <yevgeniy.engineer@gmail.com>
2023-06-24 16:46:35 +0800

    Added data types

---
+++ common/types.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <cstdint>
+
+namespace common
+{
+enum class GroundSegmentationLabels : std::uint32_t
+{
+    UNCLASSIFIED = 0,
+    GROUND,
+    OBSTACLE,
+    UNKNOWN
+};
+
+enum class ClassificationLabels : std::uint32_t
+{
+    UNCLASSIFIED = 0,
+    NOISE,
+    CAR,
+    BICYCLE,
+    BUS,
+    MOTORCYCLE,
+    TRUCK,
+    ANIMAL,
+    BIRD,
+    PEDESTRIAN,
+    BICYCLIST,
+    MOTORCYCLIST,
+    ROAD,
+    SIDEWALK,
+    BUILDING,
+    FENCE,
+    VEGETATION,
+    TERRAIN,
+    POLE,
+    TRAFFIC_SIGN
+};
+
+struct PointCartesian
+{
+    float x_m;
+    float y_m;
+    float z_m;
+    float intensity;
+};
+
+struct PointSpherical
+{
+    float range_m;
+    float azimuth_rad;
+    float elevation_rad;
+    float intensity;
+};
+} // namespace common

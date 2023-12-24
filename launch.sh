2c7fec1d89d5053739249b040ed1dd00a906f525
Yevgeniy <yevgeniy.engineer@gmail.com>
2023-12-24 20:35:41 +0800

    Small changes to launch.sh

--- launch.sh
+++ launch.sh
@@ -5,7 +5,24 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 unset GTK_PATH  # Needed for Rviz, else crashes
 
-source /opt/ros/humble/setup.bash
-source "${SCRIPT_DIR}/install/setup.bash"
+ROS_SETUP_SCRIPT="/opt/ros/humble/setup.bash"
+
+if [ -f "$ROS_SETUP_SCRIPT" ]; then
+    echo "Sourcing $ROS_SETUP_SCRIPT"
+    source "$ROS_SETUP_SCRIPT"
+else
+    echo "Warning: Unable to source $ROS_SETUP_SCRIPT, file does not exist."
+    exit 1
+fi
+
+LOCAL_SETUP_SCRIPT="${SCRIPT_DIR}/install/setup.bash"
+
+if [ -f "$LOCAL_SETUP_SCRIPT" ]; then
+    echo "Sourcing $LOCAL_SETUP_SCRIPT"
+    source "$LOCAL_SETUP_SCRIPT"
+else
+    echo "Warning: Unable to source $LOCAL_SETUP_SCRIPT, file does not exist."
+    exit 1
+fi
 
 ros2 launch lidar_camera_fusion launch.py
